d2a89d1295cbb5a4e3588c620c3d7646672c05b7
Agung Yuliaji <agung.yuliaji@gmail.com>
2016-08-24 10:32:39 +0700

    modify return clear variables

--- lib/gawe.rb
+++ lib/gawe.rb
@@ -5,6 +5,7 @@ module Gawe
   class Run
     def self.clear_variables
       TOPLEVEL_BINDING.clear
+      return "Variables cleaned"
     end
 
     singleton_class.send(:alias_method, :cv, :clear_variables)
